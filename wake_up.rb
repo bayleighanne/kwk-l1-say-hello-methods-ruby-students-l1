e3f9054dd7e0ba1c7ec5a062cb0ccde26bd6487e
bayleighanne <bayhaf718@gmail.com>
2018-06-05 16:52:36 +0000

    automatically backed up by learn

---
+++ wake_up.rb
@@ -0,0 +1,12 @@
+def wake_up
+ puts "my alram goes off"
+ puts "add 5 more minutes"
+
+ puts "get up and head to the bathroom"
+ puts "do my makeup and hair"
+ puts "eat breakfast"
+ puts "change my clothes"
+ puts "moisturize"
+end
+
+wake_up
